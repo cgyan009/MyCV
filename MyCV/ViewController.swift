02d81c7f1c707091467697fd9c12ffa88eba3718
Frank Yan <frank.v.yan@gmail.com>
2019-12-22 12:58:32 -0700

    Initial Commit

---
+++ MyCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCV
+//
+//  Created by Chenguo Yan on 2019-12-22.
+//  Copyright © 2019 Chenguo Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
