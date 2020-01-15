b6de8acd1f9c52540d30c41f29d86b89f7b79066
Frank Yan <frank.v.yan@gmail.com>
2020-01-14 20:32:47 -0600

    Add test of toString() function

--- MyCV/Models/MyCV.swift
+++ MyCV/Models/MyCV.swift
@@ -35,7 +35,7 @@ struct BasicInfo: Decodable {
 }
 
 struct Location: Decodable, CVSectionProtocol {
-  
+    
     let address: String
     let postalCode: String
     let city: String
@@ -44,15 +44,15 @@ struct Location: Decodable, CVSectionProtocol {
     private let addressString = "Address"
     
     func toString() -> String {
-          return """
-          \(addressString): \(address)
-          \(postalCode), \(city)
-          \(region), \(countryCode)
-          """
-      }
+        return """
+        \(addressString): \(address)
+        \(postalCode), \(city)
+        \(region), \(countryCode)
+        """
+    }
 }
 struct Work: Decodable, CVSectionProtocol {
- 
+    
     let company: String
     let position: String
     let website: String
@@ -63,22 +63,24 @@ struct Work: Decodable, CVSectionProtocol {
     private let hightlightsString = "Highlights"
     private let fromString = "From"
     private let toStr = "To"
+    private let positionString = "position"
     
     func toString() -> String {
-         var highlightStr = ""
-         highlights.forEach { (str) in
-             highlightStr += str
-         }
-         return """
-         \(company) \(fromString): \(startDate) \(toStr):  \(endDate)
-         \(hightlightsString): \(highlightStr)
-         \(website)
-         """
-     }
+        var highlightStr = ""
+        highlights.forEach { (str) in
+            highlightStr += str
+        }
+        return """
+        \(position)
+        \(company) \(fromString): \(startDate) \(toStr):  \(endDate)
+        \(hightlightsString): \(highlightStr)
+        \(website)
+        """
+    }
 }
 
 struct Education: Decodable, CVSectionProtocol {
-  
+    
     let institution: String
     let area: String
     let studyType: String
@@ -93,44 +95,46 @@ struct Education: Decodable, CVSectionProtocol {
     private let toStr = "To"
     
     func toString() -> String {
-          var courseString = ""
-          courses.forEach { (str) in
-              courseString += "\(str), "
-          }
-          return """
-          \(institution) \(fromString): \(startDate) \(toStr): \(endDate),
-          \(areaString): \(area),
-          \(gpaString): \(gpa),
-          \(coursesString): \(courseString)
-          """
-      }
+        var courseStr = ""
+        courses.forEach { (str) in
+            courseStr += "\(str), "
+        }
+        courseStr = String(courseStr.dropLast(2))
+        return """
+        \(institution) \(fromString): \(startDate) \(toStr): \(endDate)
+        \(areaString): \(area)
+        \(gpaString): \(gpa)
+        \(coursesString): \(courseStr)
+        """
+    }
 }
 
 struct Skill: Decodable, CVSectionProtocol {
-  
+    
     let name: String
     let level: String
     let keywords: [String]
     private let keywordsString = "Keywords"
     
     func toString() -> String {
-          var keywordString = ""
-          keywords.forEach { (str) in
-              keywordString += "\(str), "
-          }
-          return """
-          \(name), \(level),
-          \(keywordString): \(keywordString)
-          """
-      }
+        var keywordStr = ""
+        keywords.forEach { (str) in
+            keywordStr += "\(str), "
+        }
+        keywordStr = String(keywordStr.dropLast(2))
+        return """
+        \(name), \(level),
+        \(keywordsString): \(keywordStr)
+        """
+    }
 }
 struct Language: Decodable, CVSectionProtocol {
-   
+    
     let language: String
     let fluency: String
     private let fluencyString = "Fluency"
     
     func toString() -> String {
-           return "\(language), \(fluencyString): \(fluency)"
-       }
+        return "\(language), \(fluencyString): \(fluency)"
+    }
 }
--- MyCVTests/MyCVTests.swift
+++ MyCVTests/MyCVTests.swift
@@ -16,10 +16,14 @@ class MyCVTests: XCTestCase {
         mockCVApi.shouldReturnError = false
         let vm = CVViewModel(api: mockCVApi)
         let notificationExpectation = expectation(forNotification: .cvNotification, object: vm, handler: nil)
-
+        
         vm.decodeCV()
         
         wait(for: [notificationExpectation], timeout: 1)
+    }
+    
+    func testFetchCVWithError() {
+        mockFetchCV(shouldReturnError: true)
     }
     
     func testWorkToString() {
@@ -30,11 +34,64 @@ class MyCVTests: XCTestCase {
                         endDate: "2018-07-15",
                         summary: "Release Android",
                         highlights: ["Lead Flutter Project"])
-        print(work.toString())
+        let workString = """
+                        Product Manager
+                        Google From: 2010-02-21 To:  2018-07-15
+                        Highlights: Lead Flutter Project
+                        www.google.com
+                        """
+        XCTAssert(workString == work.toString())
     }
     
-    func testFetchCVWithError() {
-        mockFetchCV(shouldReturnError: true)
+    func testLocationToString() {
+        let location = Location(address: "1 Main St",
+                                postalCode: "N3R 0C5",
+                                city: "Toronto",
+                                countryCode: "CA",
+                                region: "Ontario")
+        let locationString = """
+                        Address: 1 Main St
+                        N3R 0C5, Toronto
+                        Ontario, CA
+                        """
+        XCTAssert(location.toString() == locationString)
+    }
+    
+    func testEducationToString() {
+        let education = Education(institution: "University Of Toronto",
+                                  area: "Engineering",
+                                  studyType: "Master",
+                                  startDate: "2019-4-12",
+                                  endDate: "2020-1-12",
+                                  gpa: "4.0",
+                                  courses: ["Java", "Swift", "Accounting"])
+        
+        let educationString = """
+                            University Of Toronto From: 2019-4-12 To: 2020-1-12
+                            Area: Engineering
+                            GPA: 4.0
+                            Courses: Java, Swift, Accounting
+                            """
+        
+        print(educationString)
+        XCTAssert(education.toString() == educationString)
+    }
+    
+    func testSkillToString() {
+        let skill = Skill(name: "Mobile Development", level: "Entry level", keywords: ["Coca Touch", "Java"])
+        
+        let skillString = """
+                Mobile Development, Entry level,
+                Keywords: Coca Touch, Java
+                """
+        XCTAssert(skill.toString() == skillString)
+        
+    }
+    
+    func testLanguageToString() {
+        let language = Language(language: "Franch", fluency: "Entry level")
+        let languageString = "Franch, Fluency: Entry level"
+        XCTAssert(language.toString() == languageString)
     }
     
     private func mockFetchCV(shouldReturnError: Bool) {
